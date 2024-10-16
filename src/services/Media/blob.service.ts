dfc05988e594d097c4f8d520dcd242f824c4a09a
nourabouzeid <noody081@gmail.com>
2024-10-16 22:16:47 +0300

    changed presigned url service

--- src/services/Media/blob.service.ts
+++ src/services/Media/blob.service.ts
@@ -27,18 +27,15 @@ if (!ACCOUNT_NAME) {
 if (!ACCOUNT_KEY) {
     throw new Error("Key is missing.");
 }
-// Initialize the BlobServiceClient and ContainerClient once, at the module level
+
 const blobServiceClient = BlobServiceClient.fromConnectionString(BLOB_URL);
 const containerClient = blobServiceClient.getContainerClient(CONTAINER_NAME);
 const sharedKeyCredential = new StorageSharedKeyCredential(ACCOUNT_NAME, ACCOUNT_KEY);
 
-// Function to upload a file to Azure Blob Storage
 const uploadBlob = async (file: string, blobName: string): Promise<string> => {
     try {
-        // Get a block blob client
         const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-        // Upload the file
         await blockBlobClient.uploadFile(file);
         console.log(`Blob "${blobName}" uploaded successfully`);
 
@@ -52,18 +49,16 @@ const uploadBlob = async (file: string, blobName: string): Promise<string> => {
 
 const retrieveBlob = async (blobName: string): Promise<Readable | null> => {
     try {
-        // Get a block blob client
         const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-        // Get the blob's properties to check if it exists
         const blobProperties = await blockBlobClient.getProperties();
 
         if (blobProperties) {
             console.log(`Blob "${blobName}" found. Retrieving...`);
-            const downloadBlockBlobResponse = await blockBlobClient.download(0); // Start downloading from the beginning
-            const readableStream = downloadBlockBlobResponse.readableStreamBody; // This is a Readable Stream
+            const downloadBlockBlobResponse = await blockBlobClient.download(0);
+            const readableStream = downloadBlockBlobResponse.readableStreamBody;
             if (readableStream) {
-                return readableStream as unknown as Readable; // Cast to Node.js Readable
+                return readableStream as unknown as Readable;
             }
         }
     } catch (error) {
@@ -73,7 +68,7 @@ const retrieveBlob = async (blobName: string): Promise<Readable | null> => {
         throw new Error("Error retrieving blob: " + errorMessage);
     }
 
-    return null; // Return null if the blob does not exist
+    return null;
 };
 
 const deleteBlob = async (blobName: string): Promise<string> => {
@@ -92,44 +87,26 @@ const deleteBlob = async (blobName: string): Promise<string> => {
 const getPresignedUrl = async (blobName: string, action: "read" | "write") => {
     const blobClient = containerClient.getBlobClient(blobName);
 
-    const expiryTime = new Date(new Date().valueOf() + 60 * 60 * 1000); // 1 hour expiry
+    const expiryTime = new Date(new Date().valueOf() + 60 * 60 * 1000);
 
-    if (action === "write") {
-        const permissions = new BlobSASPermissions();
-        permissions.create = true; // Allow create permission for upload
-        permissions.write = true; // Allow create permission for upload
+    const permissions = new BlobSASPermissions();
+    if (action == "write") {
+        permissions.create = true;
+        permissions.write = true;
+    } else permissions.read = true;
 
-        const sasToken = generateBlobSASQueryParameters(
-            {
-                containerName: CONTAINER_NAME,
-                blobName,
-                permissions,
-                expiresOn: expiryTime,
-            },
-            sharedKeyCredential
-        ).toString();
+    const sasToken = generateBlobSASQueryParameters(
+        {
+            containerName: CONTAINER_NAME,
+            blobName,
+            permissions,
+            expiresOn: expiryTime,
+        },
+        sharedKeyCredential
+    ).toString();
 
-        // The full SAS URL
-        const presignedUrl = `https://${ACCOUNT_NAME}.blob.core.windows.net/${CONTAINER_NAME}/${blobName}?${sasToken}`;
-        return presignedUrl;
-    } else {
-        const permissions = new BlobSASPermissions();
-        permissions.read = true; // Allow read permission for streaming
-
-        const sasToken = generateBlobSASQueryParameters(
-            {
-                containerName: CONTAINER_NAME,
-                blobName,
-                permissions,
-                expiresOn: expiryTime,
-            },
-            sharedKeyCredential
-        ).toString();
-
-        // The full SAS URL
-        const presignedUrl = `https://${ACCOUNT_NAME}.blob.core.windows.net/${CONTAINER_NAME}/${blobName}?${sasToken}`;
-        return presignedUrl;
-    }
+    const presignedUrl = `https://${ACCOUNT_NAME}.blob.core.windows.net/${CONTAINER_NAME}/${blobName}?${sasToken}`;
+    return presignedUrl;
 };
 
 export { uploadBlob, retrieveBlob, deleteBlob, getPresignedUrl };
