6357f9d2aa22ca790977d3a02d03277e3a5d4de2
Youssef Tarek <myoussef9366@gmail.com>
2024-10-16 16:36:53 +0300

    Rename expiring event to expire instead of delete

--- src/socket/handlers/message.handlers.ts
+++ src/socket/handlers/message.handlers.ts
@@ -4,35 +4,35 @@ import { deleteMessage } from "@controllers/message-controller/delete.message";
 import { getChatParticipantsIds } from "@services/chat-service/chat.participant.service";
 
 export const broadCast = async (
-  chatId: number,
-  clients: Map<number, Socket>,
-  emitEvent: string,
-  emitMessage: any
+    chatId: number,
+    clients: Map<number, Socket>,
+    emitEvent: string,
+    emitMessage: any
 ): Promise<void> => {
-  try {
-    const participants: number[] = await getChatParticipantsIds(chatId);
+    try {
+        const participants: number[] = await getChatParticipantsIds(chatId);
 
-    participants &&
-      participants.forEach((participant) => {
-        if (clients.has(participant)) {
-          const client = clients.get(participant);
-          if (client) {
-            client.emit(emitEvent, emitMessage);
-          }
-        }
-      });
-  } catch (error) {
-    console.error(error);
-  }
+        participants &&
+            participants.forEach((participant) => {
+                if (clients.has(participant)) {
+                    const client = clients.get(participant);
+                    if (client) {
+                        client.emit(emitEvent, emitMessage);
+                    }
+                }
+            });
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 export const notifyExpiry = async (key: string, clients: Map<number, Socket>): Promise<void> => {
-  const match = key.match(/\d+/);
-  if (!match) return;
+    const match = key.match(/\d+/);
+    if (!match) return;
 
-  const id: number = Number(match[0]);
-  const chatId: number = await getChatId(id);
-  await deleteMessage(id, chatId);
+    const id: number = Number(match[0]);
+    const chatId: number = await getChatId(id);
+    await deleteMessage(id, chatId);
 
-  broadCast(chatId, clients, "delete", id);
+    broadCast(chatId, clients, "expire", id);
 };
