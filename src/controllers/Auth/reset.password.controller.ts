93e0c6874efaed87e47c4dbb7c8c6ebe2dc7955e
nourabouzeid <noody081@gmail.com>
2024-10-21 20:29:31 +0300

    logs in when reset password

--- src/controllers/Auth/reset.password.controller.ts
+++ src/controllers/Auth/reset.password.controller.ts
@@ -2,8 +2,11 @@ import { validateEmail, validateResetCode } from "@validators/confirm.reset";
 import { Request, Response } from "express";
 import { updatePassword } from "@services/auth/reset.password.service";
 import { verifyCode, createCode, sendCode } from "@services/auth/confirmation.service";
-import { checkEmailExist } from "@services/auth/login.service";
+import { checkEmailExist, incrementUserDevices } from "@services/auth/login.service";
 import RedisOperation from "@src/@types/redis.operation";
+import { User } from "@prisma/client";
+import jwt from "jsonwebtoken";
+import { createCookie } from "@services/auth/cookie.service";
 
 async function sendResetCode(req: Request, res: Response) {
     try {
@@ -40,10 +43,22 @@ async function resetPassword(req: Request, res: Response) {
 
         await verifyCode(email, code, RedisOperation.ResetPassword);
 
-        updatePassword(email, password);
+        const user: User = await updatePassword(email, password);
+        const userToken: string = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, {
+            expiresIn: process.env.JWT_EXPIRE,
+        });
 
+        incrementUserDevices(user.id);
+        createCookie(res, userToken);
         res.status(200).json({
             status: "success",
+            user: {
+                id: user.id,
+                name: user.name,
+                userName: user.userName,
+                email: user.email,
+            },
+            userToken,
         });
     } catch (e: any) {
         console.log(e.message);
--- src/services/Auth/reset.password.service.ts
+++ src/services/Auth/reset.password.service.ts
@@ -1,14 +1,15 @@
-import db from '@DB';
-import bcrypt from 'bcrypt';
-
+import db from "@DB";
+import { User } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 async function updatePassword(email: string, password: string) {
-    await db.user.update({
+    const user: User | null = await db.user.update({
         where: { email },
         data: {
             password: bcrypt.hashSync(password, 10),
-            loggedInDevices: 0
-        }
+            loggedInDevices: 0,
+        },
     });
+    return user;
 }
 export { updatePassword };
