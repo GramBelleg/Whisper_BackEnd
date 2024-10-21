e19c119369045e7ccfd7482a52cd60086bdbf347
nourabouzeid <noody081@gmail.com>
2024-10-21 18:59:00 +0300

    added authorization through query in socket

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Request } from "express";
-import db from '@DB';
+import db from "@DB";
 
 function getToken(req: Request) {
     let token: string;
@@ -15,13 +15,13 @@ function getToken(req: Request) {
 
 async function checkLogoutAll(userId: number) {
     const user = await db.user.findUnique({
-        where: { id: userId }
+        where: { id: userId },
     });
     if (!user) {
-        throw new Error('User is not existed');
+        throw new Error("User is not existed");
     }
     if (user.loggedInDevices <= 0) {
-        throw new Error('All devices are logged out. Login again');
+        throw new Error("All devices are logged out. Login again");
     }
 }
 
