79384635cbd04ce68db0665911b0369c7e66d3ca
nourabouzeid <noody081@gmail.com>
2024-10-24 00:41:53 +0300

    insert user into redis unit test passed

--- src/__tests__/unit/services/auth/auth.service.test.ts
+++ src/__tests__/unit/services/auth/auth.service.test.ts
@@ -1,46 +1,46 @@
-// import bcrypt from "bcrypt";
-// import { saveUserData } from "@services/auth/signup.service"; // Import the function to test
-// import RedisOperation from "@src/@types/redis.operation";
-// import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import { saveUserData } from "@services/auth/signup.service"; // Import the function to test
+import { checkEmailExistRedis, getTimeToLive } from "@services/auth/confirmation.service";
+import dotenv from "dotenv";
 
-// dotenv.config({ path: "test.env" });
+dotenv.config({ path: "test.env" });
 
-// describe("saveUserData function", () => {
-//     beforeAll(async () => {});
+describe("saveUserData function", () => {
+    beforeAll(async () => {});
 
-//     afterAll(async () => {});
+    afterAll(async () => {});
 
-//     it("should store user data in Redis with an expiration time", async () => {
-//         const email = "testuser@example.com";
-//         const userData = {
-//             name: "Test User",
-//             userName: "testuser",
-//             email: email,
-//             phoneNumber: "1234567890",
-//             password: "testpassword",
-//         };
+    it("should store user data in Redis with an expiration time", async () => {
+        const email = "testuser@example.com";
+        const userData = {
+            name: "Test User",
+            userName: "testuser",
+            email: email,
+            phoneNumber: "1234567890",
+            password: "testpassword",
+        };
 
-//         // Call the saveUserData function
-//         const storedData = await saveUserData(
-//             userData.name,
-//             userData.userName,
-//             userData.email,
-//             userData.phoneNumber,
-//             userData.password
-//         );
+        // Call the saveUserData function
+        await saveUserData(
+            userData.name,
+            userData.userName,
+            userData.email,
+            userData.phoneNumber,
+            userData.password
+        );
+        const storedData = await checkEmailExistRedis(userData.email);
+        // Verify that the data was stored in Redis
 
-//         // Verify that the data was stored in Redis
+        expect(storedData.name).toEqual(userData.name);
+        expect(storedData.userName).toEqual(userData.userName);
+        expect(storedData.email).toEqual(userData.email);
+        expect(storedData.phoneNumber).toEqual(userData.phoneNumber);
+        // Ensure that the password is hashed
+        expect(bcrypt.compareSync(userData.password, storedData.password)).toBe(true);
 
-//         expect(storedData.name).toEqual(userData.name);
-//         expect(storedData.userName).toEqual(userData.userName);
-//         expect(storedData.email).toEqual(userData.email);
-//         expect(storedData.phoneNumber).toEqual(userData.phoneNumber);
-//         // Ensure that the password is hashed
-//         expect(bcrypt.compareSync(userData.password, storedData.password)).toBe(true);
-
-//         // Verify the TTL (Time to Live) is set to 3 hours (10800 seconds)
-//         const ttl = await redis.ttl(`${RedisOperation.AddNewUser}:${email}`);
-//         expect(ttl).toBeGreaterThan(0); // TTL should be greater than 0 if it was set correctly
-//         expect(ttl).toBeLessThanOrEqual(10800); // Shouldn't be more than 3 hours
-//     });
-// });
+        // Verify the TTL (Time to Live) is set to 3 hours (10800 seconds)
+        const ttl = await getTimeToLive(userData.email);
+        expect(ttl).toBeGreaterThan(0); // TTL should be greater than 0 if it was set correctly
+        expect(ttl).toBeLessThanOrEqual(10800); // Shouldn't be more than 3 hours
+    });
+});
