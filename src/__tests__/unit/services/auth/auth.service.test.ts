e24db7d732a9d11f9de6b897ace16b14fc914a75
nourabouzeid <noody081@gmail.com>
2024-10-24 03:12:46 +0300

    close connection after unit test

--- src/__tests__/unit/services/auth/auth.service.test.ts
+++ src/__tests__/unit/services/auth/auth.service.test.ts
@@ -2,14 +2,14 @@ import bcrypt from "bcrypt";
 import { saveUserData } from "@services/auth/signup.service"; // Import the function to test
 import { checkEmailExistRedis, getTimeToLive } from "@services/auth/confirmation.service";
 import dotenv from "dotenv";
+import redis from "@src/redis/redis.client"; // Import the redis client
 
 dotenv.config({ path: "test.env" });
 
 describe("saveUserData function", () => {
-    beforeAll(async () => {});
-
-    afterAll(async () => {});
-
+    afterAll(async () => {
+        await redis.quit();
+    });
     it("should store user data in Redis with an expiration time", async () => {
         const email = "testuser@example.com";
         const userData = {
@@ -20,7 +20,6 @@ describe("saveUserData function", () => {
             password: "testpassword",
         };
 
-        // Call the saveUserData function
         await saveUserData(
             userData.name,
             userData.userName,
@@ -29,18 +28,15 @@ describe("saveUserData function", () => {
             userData.password
         );
         const storedData = await checkEmailExistRedis(userData.email);
-        // Verify that the data was stored in Redis
 
         expect(storedData.name).toEqual(userData.name);
         expect(storedData.userName).toEqual(userData.userName);
         expect(storedData.email).toEqual(userData.email);
         expect(storedData.phoneNumber).toEqual(userData.phoneNumber);
-        // Ensure that the password is hashed
         expect(bcrypt.compareSync(userData.password, storedData.password)).toBe(true);
 
-        // Verify the TTL (Time to Live) is set to 3 hours (10800 seconds)
         const ttl = await getTimeToLive(userData.email);
-        expect(ttl).toBeGreaterThan(0); // TTL should be greater than 0 if it was set correctly
-        expect(ttl).toBeLessThanOrEqual(10800); // Shouldn't be more than 3 hours
+        expect(ttl).toBeGreaterThan(0);
+        expect(ttl).toBeLessThanOrEqual(10800);
     });
 });
