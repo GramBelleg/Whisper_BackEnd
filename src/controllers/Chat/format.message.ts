9c67c094a17d8e55dcc2080797c7953e9460e5b9
Youssef Tarek <myoussef9366@gmail.com>
2024-10-28 13:40:21 +0300

    nothing

--- src/controllers/Chat/format.message.ts
+++ src/controllers/Chat/format.message.ts
@@ -2,11 +2,11 @@ import { Message } from "@prisma/client";
 import { ReceivedMessage } from "@models/chat.models";
 import { getMessageStatus, getMessageSummary } from "@services/chat/message.service";
 
-export const buildReceivedMessage = async (userId: number, message: Message): Promise<ReceivedMessage> => {
+export const buildReceivedMessage = async (userId: number, message: Message): Promise<ReceivedMessage | null> => {
     const parentMessageId = message.parentMessageId;
     const parentMessage = parentMessageId ? await getMessageSummary(parentMessageId) : null;
-    const time = (await getMessageStatus(userId, message.id)) as { time: Date };
-
+    const time = (await getMessageStatus(userId, message.id));
+    if(!time) return null;
     return {
         parentMessage,
         ...message,
