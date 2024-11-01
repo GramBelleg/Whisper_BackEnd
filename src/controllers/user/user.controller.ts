1c42b56a39691945aabe07114f908d69c4cbd23f
KareemAshrafSaeed <kkaarreemm2002@gmail.com>
2024-11-01 15:59:52 +0200

    remove (try/catch) block user.controller

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -2,60 +2,41 @@ import { Request, Response } from "express";
 import * as userServices from "@services/user/user.service";
 import { validateEmail } from "@validators/confirm.reset";
 import { sendCode } from "@services/auth/confirmation.service";
+import {createCode} from "@services/auth/confirmation.service"
 import { findEmail } from "@services/auth/signup.service";
 import RedisOperation from "@src/@types/redis.operation";
 import HttpError from "@src/errors/HttpError";
 
 const updateBio = async (req: Request, res: Response) => {
-    try {
-        let { bio = "" }: { bio: string } = req.body;
-        let id: number = req.userId;
-        await userServices.updateBio(id, bio);
-        res.status(200).json({
-            status: "success",
-            data: bio,
-        });
-    } catch (e: any) {
-        res.status(400).json({
-            status: "failed",
-            message: e.message,
-        });
-    }
+    let { bio = "" }: { bio: string } = req.body;
+    let id: number = req.userId;
+    await userServices.updateBio(id, bio);
+    res.status(200).json({
+        status: "success",
+        data: bio,
+    });
 };
 
 const updateName = async (req: Request, res: Response) => {
-    try {
-        let { name = "" }: { name: string } = req.body;
-        let id: number = req.userId;
-        await userServices.updateName(id, name);
-        res.status(200).json({
-            status: "success",
-            data: name,
-        });
-    } catch (e: any) {
-        res.status(400).json({
-            status: "failed",
-            message: e.message,
-        });
-    }
+    let { name = "" }: { name: string } = req.body;
+    let id: number = req.userId;
+    await userServices.updateName(id, name);
+    res.status(200).json({
+        status: "success",
+        data: name,
+    });
 };
 
 const updateEmail = async (req: Request, res: Response) => {
     let { email = "", code = "" }: { email: string; code: string } = req.body;
     let id: number = req.userId;
-    try {
-        await userServices.updateEmail(id, email, code);
-        res.status(200).json({
-            status: "success",
-            data: email,
-        });
-    } catch (e: any) {
-        res.status(400).json({
-            status: "failed",
-            message: e.message,
-        });
-    }
+    await userServices.updateEmail(id, email, code);
+    res.status(200).json({
+        status: "success",
+        data: email,
+    });
 };
+
 const emailCode = async (req: Request, res: Response): Promise<void> => {
     const { email } = req.body as Record<string, string>;
     validateEmail(email);
@@ -63,8 +44,8 @@ const emailCode = async (req: Request, res: Response): Promise<void> => {
     const foundEmail = await findEmail(email);
     if (foundEmail) throw new HttpError("Email already exists", 409);
 
-    const code = await userServices.createCode(email, RedisOperation.ConfirmEmail);
-    const emailBody = `<h3>Hello, </h3> <p>Thanks for joining our family. Use this code: <b>${code}</b> for verifing your email</p>`;
+    const code = await createCode(email, RedisOperation.ConfirmEmail, 300);
+    const emailBody = `<h3>Hello, </h3> <p>Thanks for joining our family. Use this code: <b>${code}</b> for verifying your email</p>`;
     await sendCode(email, "confirmation code", emailBody);
 
     res.status(200).json({
@@ -73,71 +54,41 @@ const emailCode = async (req: Request, res: Response): Promise<void> => {
 };
 
 const updatePhone = async (req: Request, res: Response) => {
-    //TODO: fix Phone number structure is not valid
-    try {
-        let { phoneNumber = "" }: { phoneNumber: string } = req.body;
-        let id: number = req.userId;
-        let updatedPhone = await userServices.updatePhone(id, phoneNumber);
-        res.status(200).json({
-            status: "success",
-            data: updatedPhone,
-        });
-    } catch (e: any) {
-        res.status(400).json({
-            status: "failed",
-            message: e.message,
-        });
-    }
+    let { phoneNumber = "" }: { phoneNumber: string } = req.body;
+    let id: number = req.userId;
+    let updatedPhone = await userServices.updatePhone(id, phoneNumber);
+    res.status(200).json({
+        status: "success",
+        data: updatedPhone,
+    });
 };
 
 const changePic = async (req: Request, res: Response) => {
-    //to delete profilePic blobName = "profilePic.jpg"
     const id: number = req.userId;
     const blob = req.body.blobName;
-    try {
-        await userServices.changePic(id, blob);
-        res.status(200).json({
-            status: "success",
-            name: blob,
-        });
-    } catch (e: any) {
-        res.status(400).json({
-            status: "failed",
-            message: e.message,
-        });
-    }
+    await userServices.changePic(id, blob);
+    res.status(200).json({
+        status: "success",
+        name: blob,
+    });
 };
 
 const userInfo = async (req: Request, res: Response) => {
-    try {
-        let user = await userServices.userInfo(req.body.email);
-        res.status(200).json({
-            status: "success",
-            data: user,
-        });
-    } catch (e: any) {
-        res.status(400).json({
-            status: "failed",
-            message: e.message,
-        });
-    }
+    let user = await userServices.userInfo(req.body.email);
+    res.status(200).json({
+        status: "success",
+        data: user,
+    });
 };
 
 const changeUserName = async (req: Request, res: Response) => {
-    try {
-        let { userName = "" }: { userName: string } = req.body;
-        let id: number = req.userId;
-        await userServices.changeUserName(id, userName);
-        res.status(200).json({
-            status: "success",
-            data: userName,
-        });
-    } catch (e: any) {
-        res.status(400).json({
-            status: "failed",
-            message: e.message,
-        });
-    }
+    let { userName = "" }: { userName: string } = req.body;
+    let id: number = req.userId;
+    await userServices.changeUserName(id, userName);
+    res.status(200).json({
+        status: "success",
+        data: userName,
+    });
 };
 
 export {
--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -5,7 +5,6 @@ import RedisOperation from "@src/@types/redis.operation";
 import { saveStory } from "@services/redis/story.service";
 import { Story } from "@prisma/client";
 import { SaveableStory } from "@models/story.models";
-import redis from "@src/redis/redis.client";
 
 const updateBio = async (id: number, bio: string): Promise<string> => {
     try {
@@ -144,15 +143,6 @@ const getUserId = async (userName: string): Promise<number | null> => {
     return result.id;
 };
 
-const createCode = async (email: string, operation: RedisOperation) => {
-    const firstCode: string = Randomstring.generate(8);
-    const code = firstCode.replace(/[Il]/g, "s");
-    const expireAt = new Date(Date.now() + 300000).toString(); // after 5 minutes
-
-    await redis.hmset(`${operation}:${code}`, { email, expireAt });
-    await redis.expire(`${operation}:${code}`, 600); // expire in 10 minutes
-    return code;
-};
 export {
     setStory,
     userInfo,
@@ -163,5 +153,4 @@ export {
     changePic,
     changeUserName,
     getUserId,
-    createCode,
 };
