64a84e1615cd02fcc58da07d1dae16f74e4c1394
nourabouzeid <noody081@gmail.com>
2024-11-01 16:29:35 +0200

    added google redirect to mobile

--- src/controllers/auth/google.auth.controller.ts
+++ src/controllers/auth/google.auth.controller.ts
@@ -5,41 +5,43 @@ import { createTokenCookie, createAddToken } from "@services/auth/token.service"
 import { User } from "@prisma/client";
 
 async function googleAuth(req: Request, res: Response): Promise<void> {
-    try {
-        const authCode: string | undefined = req.body.code;
-        if (!authCode) {
-            throw new Error("There is no Authorization Code");
-        }
-
-        const accessToken = await getAccessToken(authCode);
-
-        const userData: Record<string, any> | undefined = await getUserData(accessToken);
-        if (!userData) {
-            throw new Error("Invalid Access Token");
-        }
-
-        const user: User = await upsertUser(userData);
-
-        const userToken = await createAddToken(user.id);
-        //*IMPORTANT* Make sure frontend sets withcredential: true so that the cookies are sent with the request
-        createTokenCookie(res, userToken);
-
-        res.status(200).json({
-            status: "success",
-            user: {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-            },
-            userToken,
-        });
-    } catch (err: any) {
-        console.log(err.message);
-        res.status(400).json({
-            status: "failed",
-            message: err.message,
-        });
+    const authCode: string | undefined = req.body.code;
+    if (!authCode) {
+        throw new Error("There is no Authorization Code");
     }
+
+    const accessToken = await getAccessToken(authCode);
+
+    const userData: Record<string, any> | undefined = await getUserData(accessToken);
+    if (!userData) {
+        throw new Error("Invalid Access Token");
+    }
+
+    const user: User = await upsertUser(userData);
+
+    const userToken = await createAddToken(user.id);
+    //*IMPORTANT* Make sure frontend sets withcredential: true so that the cookies are sent with the request
+    createTokenCookie(res, userToken);
+
+    res.status(200).json({
+        status: "success",
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        },
+        userToken,
+    });
 }
 
-export default googleAuth;
+async function googleRedirect(req: Request, res: Response): Promise<void> {
+    const { authCode } = req.query;
+    if (!authCode) {
+        throw new Error("There is no Authorization Code");
+    }
+
+    const redirectUri = `whisperSeifMohamed://oauth2redirect/?code=${authCode}`;
+    res.redirect(redirectUri);
+}
+
+export { googleAuth, googleRedirect };
--- src/routes/authentication.routes.ts
+++ src/routes/authentication.routes.ts
@@ -3,7 +3,7 @@ import asyncHandler from "express-async-handler";
 import login from "@controllers/auth/login.controller";
 import signup from "@controllers/auth/signup.controller";
 import { resendConfirmCode, confirmEmail } from "@controllers/auth/confirmation.controller";
-import googleAuth from "@controllers/auth/google.auth.controller";
+import { googleAuth, googleRedirect } from "@controllers/auth/google.auth.controller";
 import { githubAuth } from "@controllers/auth/github.auth.controller";
 import { facebookAuth } from "@controllers/auth/facebook.auth.controller";
 import { resetPassword, sendResetCode } from "@controllers/auth/reset.password.controller";
@@ -17,6 +17,7 @@ router.post("/confirmEmail", asyncHandler(confirmEmail));
 router.post("/sendResetCode", asyncHandler(sendResetCode));
 router.post("/resetPassword", asyncHandler(resetPassword));
 router.post("/google", asyncHandler(googleAuth));
+router.get("/google/redirect", asyncHandler(googleRedirect));
 router.post("/facebook", asyncHandler(facebookAuth));
 router.post("/github", asyncHandler(githubAuth));
 
