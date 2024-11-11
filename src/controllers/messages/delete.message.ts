23341acb553c0ce6f96248e55df2368c08deb941
nourabouzeid <noody081@gmail.com>
2024-11-11 16:45:18 +0200

    removed elastic search

--- src/controllers/messages/delete.message.ts
+++ src/controllers/messages/delete.message.ts
@@ -1,17 +1,12 @@
 import { Request, Response } from "express";
 import { deleteMessagesForUser, deleteMessagesForAll } from "@services/chat/message.service";
 import { setNewLastMessage } from "@services/chat/chat.service";
-import {
-    deleteMessagesForAllInES,
-    deleteMessagesforUserInES,
-} from "@services/elasticsearch/message.service";
 
 export const deleteMessagesForCurrentUser = async (req: Request, res: Response) => {
     const userId = req.userId;
     const Ids = JSON.parse(req.query.Ids as string).map(Number);
     const chatId = Number(req.params.chatId);
     await deleteMessagesForUser(userId, Ids);
-    await deleteMessagesforUserInES(userId, Ids);
     await setNewLastMessage(chatId);
     res.status(200).json({ Message: "Messages deleted successfully" });
 };
@@ -19,7 +14,6 @@ export const deleteMessagesForCurrentUser = async (req: Request, res: Response) 
 export const deleteMessagesForAllUsers = async (Ids: number[], chatId: number) => {
     try {
         await deleteMessagesForAll(Ids);
-        await deleteMessagesForAllInES(Ids);
         await setNewLastMessage(chatId);
     } catch (error) {
         console.error("Error deleting message:", error);
--- src/controllers/messages/search.messages.ts
+++ src/controllers/messages/search.messages.ts
@@ -1,6 +1,4 @@
 import { Request, Response } from "express";
-import { searchMessagesInES } from "@services/elasticsearch/message.service";
-import { MessageIndex } from "@models/es.models";
 
 const formatMessages = (messages: MessageIndex[]) => {
     return messages.map((message) => {
@@ -22,7 +20,5 @@ export const handleSearchMessages = async (req: Request, res: Response) => {
     const userId = req.userId;
     const chatId = Number(req.params.chatId);
     const query = req.query.query as string;
-    const messages = await searchMessagesInES(userId, chatId, query);
-    const results = formatMessages(messages);
-    res.status(200).json(results);
+    res.status(200).json();
 };
