5dc5572411c8ae3bb4295cf22e6468eeb7e7353c
nourabouzeid <noody081@gmail.com>
2024-11-23 19:05:04 +0200

    :sparkles: feat: sticker functionality add: sticker table sticker routes, controllers, and services to get and add stickers

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -5,6 +5,7 @@ import chatsRouter from "./chat.routes";
 import userAuth from "@middlewares/auth.middleware";
 import mediaRouter from "@routes/media.routes";
 import messagesRouter from "@routes/message.routes";
+import stickersRouter from "@routes/sticker.routes";
 import eventsRouter from "@routes/events.routes";
 const router: Router = Router();
 
@@ -15,5 +16,6 @@ router.use("/user", userRouter);
 router.use("/media", mediaRouter);
 router.use("/chats", chatsRouter);
 router.use("/messages", messagesRouter);
+router.use("/stickers", stickersRouter);
 
 export default router;
--- src/services/user/story.service.ts
+++ src/services/user/story.service.ts
@@ -1,19 +1,23 @@
 import db from "@DB";
-
-export const getStoryParticipant = async (userId: number, except: Array<number> = []): Promise<number[]> => {
+export const getStoryParticipant = async (
+    userId: number,
+    except: Array<number> = []
+): Promise<number[]> => {
     const contacts = await db.chat.findMany({
         where: {
             participants: {
                 some: {
                     userId,
                     isContact: true,
-                    NOT: { userId: { in: [...except, userId] } }
-                }
+                    NOT: { userId: { in: [...except, userId] } },
+                },
             },
-            type: "DM"
+            type: "DM",
         },
         select: { participants: { select: { userId: true } } },
     });
-    const results = contacts.flatMap((contact) => contact.participants.map((participant) => participant.userId));
+    const results = contacts.flatMap((contact) =>
+        contact.participants.map((participant) => participant.userId)
+    );
     return results;
 };
