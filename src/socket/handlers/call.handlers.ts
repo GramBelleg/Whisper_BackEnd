9296181e2751dec095d42ab3ea4d3da978fd2cfc
KareemAshrafSaeed <kkaarreemm2002@gmail.com>
2024-12-12 16:28:51 +0200

    :ambulance: edit the call socket

--- src/socket/handlers/call.handlers.ts
+++ src/socket/handlers/call.handlers.ts
@@ -8,6 +8,6 @@ export const call = async (
     channelName: string
 ): Promise<void> => {
     for (let i = 0; i < participants.length; i++) {
-        sendToClient(participants[i], clients, "call", {token: tokens[i], chatId: channelName});
+        sendToClient(participants[i], clients, "call", {token: tokens[i], channelName: channelName});
     }
 };
