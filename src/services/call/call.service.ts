2078abb5d00c448b0fb3e0df16807cc9cac56679
KareemAshrafSaeed <kkaarreemm2002@gmail.com>
2024-12-19 21:40:31 +0200

    :pencil2: cancelled

--- src/services/call/call.service.ts
+++ src/services/call/call.service.ts
@@ -181,7 +181,7 @@ export const leaveCall = async (chatId: string, endStatus: any) => {
     }
     let content = "Call Ended";
 
-    if (endStatus === "CANCELED") {
+    if (endStatus === "CANCELLED") {
         content = "Call Canceled";
         cancelCall(participants, { chatId: lastCall.chatId });
     }
